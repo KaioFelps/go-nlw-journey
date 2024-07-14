5a552399b24b8be83cfee3431f4c771b66fe2fd2
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-07-14 18:23:18 -0300

    chore gen.go

--- gen.go
+++ gen.go
@@ -2,5 +2,6 @@ package main
 
 //go:generate go run .\cmd\etern\main.go migrate
 //go:generate sqlc generate -f ./internal/pgstore/sqlc.yaml
+//go:generate goapi-gen --package=spec --out internal/api/spec/journey.gen.spec.go internal/api/spec/journey.spec.json
 
 // rode tudo isso com o comando `go generate ./ ...`
